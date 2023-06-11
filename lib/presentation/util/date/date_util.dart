0691d3d552b5663cbf51ffdd0774e2f6ac67414d
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-06-12 06:41:29 +0900

    Feature : Add getTimeAgo function for converting date to relative time

---
+++ lib/presentation/util/date/date_util.dart
@@ -0,0 +1,41 @@
+class DateUtil {
+
+  String getTimeAgo(String dateTimeString) {
+    DateTime dateTime = DateTime.parse(dateTimeString);
+    DateTime now = DateTime.now();
+    Duration difference = now.difference(dateTime);
+
+    if (difference.inSeconds < 60) {
+      if (difference.inSeconds == 1) {
+        return '1 second ago';
+      } else {
+        return '${difference.inSeconds} seconds ago';
+      }
+    } else if (difference.inMinutes < 60) {
+      if (difference.inMinutes == 1) {
+        return '1 minute ago';
+      } else {
+        return '${difference.inMinutes} minutes ago';
+      }
+    } else if (difference.inHours < 24) {
+      if (difference.inHours == 1) {
+        return '1 hour ago';
+      } else {
+        return '${difference.inHours} hours ago';
+      }
+    } else if (difference.inDays < 7) {
+      if (difference.inDays == 1) {
+        return '1 day ago';
+      } else {
+        return '${difference.inDays} days ago';
+      }
+    } else {
+      final weeks = difference.inDays ~/ 7;
+      if (weeks == 1) {
+        return '1 week ago';
+      } else {
+        return '$weeks weeks ago';
+      }
+    }
+  }
+}
