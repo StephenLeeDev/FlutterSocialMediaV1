54718235d477379cf2ff3fc3a542ec4a5c169043
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-10-08 02:03:33 +0900

    Test : Unit test [getDateString] function
    - Write test code for the function [getDateString]

--- test/util/date/date_util_test.dart
+++ test/util/date/date_util_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_social_media_v1/presentation/util/date/date_util.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  group('DateUtil', () {
-    test('getTimeAgo', () {
+  group('getTimeAgo', () {
+    test('return time ago by DateTime', () {
       final now = DateTime.now();
 
       testTimeAgo('Less than 1 minute ago', now.subtract(const Duration(seconds: 1)).toString());
@@ -18,10 +19,39 @@ void main() {
       testTimeAgo('30 weeks ago', now.subtract(const Duration(days: 210)).toString());
     });
   });
+
+  group('getDateString', () {
+    test('return date string by DateTime', () {
+      final now = DateTime(2023, 10, 8, 10, 30);
+
+      /// Today
+      testDateString("10:30 AM", now);
+      testDateString("10:30 PM", now.add(const Duration(hours: 12)));
+
+      /// Yesterday
+      DateTime yesterday = now.subtract(const Duration(days: 1));
+      testDateString("Yesterday", yesterday);
+
+      /// This year
+      DateTime twoDaysAgo = now.subtract(const Duration(days: 2));
+      DateTime lastMonth = now.subtract(const Duration(days: 14));
+      testDateString("October 6", twoDaysAgo);
+      testDateString("September 24", lastMonth);
+
+      /// Last year
+      testDateString("2022.10.08", now.subtract(const Duration(days: 365)));
+    });
+  });
 }
 
 void testTimeAgo(String expected, String dateTimeString) {
   final dateUtil = DateUtil();
   final result = dateUtil.getTimeAgo(dateTimeString);
+  expect(result, expected);
+}
+
+void testDateString(String expected, DateTime dateTime) {
+  final dateUtil = DateUtil();
+  final result = dateUtil.getDateString(dateTime);
   expect(result, expected);
 }
